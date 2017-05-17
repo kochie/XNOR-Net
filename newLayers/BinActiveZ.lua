436d2f520909adff0d927a992dd928284584cfe7
Robert Koch <robert@kochie.io>
2017-05-17 10:23:57 +1000

    Added ternary network draft.

--- newLayers/BinActiveZ.lua
+++ newLayers/BinActiveZ.lua
@@ -1,4 +1,5 @@
 local BinActiveZ , parent= torch.class('nn.BinActiveZ', 'nn.Module')
+local TriActiveZ, parent= torch.class('nn.TriActiveZ', 'nn.Module')
 
 
 function BinActiveZ:updateOutput(input)
@@ -9,6 +10,23 @@ function BinActiveZ:updateOutput(input)
 end
 
 function BinActiveZ:updateGradInput(input, gradOutput)
+   local s = input:size()
+   self.gradInput:resizeAs(gradOutput):copy(gradOutput)
+   self.gradInput[input:ge(1)]=0
+   self.gradInput[input:le(-1)]=0
+   return self.gradInput
+end
+
+function TriActiveZ:updateOutput(input)
+	local s = input:size()
+		self.output:resizeAs(input):copy(input)
+		self.output[input:ge(0.1)]=1
+		self.output[input:le(0.1)]=0
+    self.output[input:le(-0.1)]=-1
+		return self.output
+end
+
+function TriActiveZ:updateGradInput(input, gradOutput)
    local s = input:size()
    self.gradInput:resizeAs(gradOutput):copy(gradOutput)
    self.gradInput[input:ge(1)]=0
