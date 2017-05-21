7f3c3c0b8909f2ba355bcb0ccfe7cbebdd74c92f
Robert Koch <robert@kochie.io>
2017-05-21 23:44:56 +1000

    Added options for ternary network.

--- models/alexnetxnor.lua
+++ models/alexnetxnor.lua
@@ -2,7 +2,7 @@ function createModel()
    require 'cudnn'
       local function activation()
       local C= nn.Sequential()
-      C:add(nn.BinActiveZ())
+      C:add(nn.TriActiveZ())
       return C
    end
 
@@ -14,7 +14,7 @@ function createModel()
          local C= nn.Sequential()
           C:add(nn.SpatialBatchNormalization(nInputPlane,1e-4,false))
           C:add(activation())
-   		    C:add(cudnn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH))   
+   		    C:add(cudnn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH))
    		 return C
    end
 
@@ -26,19 +26,19 @@ function createModel()
           C:add(MaxPooling(3,3,2,2))
        return C
    end
-   
+
 local features = nn.Sequential()
-   
-   features:add(cudnn.SpatialConvolution(3,96,11,11,4,4,2,2))      
+
+   features:add(cudnn.SpatialConvolution(3,96,11,11,4,4,2,2))
    features:add(nn.SpatialBatchNormalization(96,1e-5,false))
    features:add(cudnn.ReLU(true))
    features:add(MaxPooling(3,3,2,2))
 
-   features:add(BinMaxConvolution(96,256,5,5,1,1,2,2))        
-   features:add(BinConvolution(256,384,3,3,1,1,1,1))      
-   features:add(BinConvolution(384,384,3,3,1,1,1,1)) 
-   features:add(BinMaxConvolution(384,256,3,3,1,1,1,1))        
-   features:add(BinConvolution(256,4096,6,6))        
+   features:add(BinMaxConvolution(96,256,5,5,1,1,2,2))
+   features:add(BinConvolution(256,384,3,3,1,1,1,1))
+   features:add(BinConvolution(384,384,3,3,1,1,1,1))
+   features:add(BinMaxConvolution(384,256,3,3,1,1,1,1))
+   features:add(BinConvolution(256,4096,6,6))
    features:add(BinConvolution(4096,4096,1,1))
 
    features:add(nn.SpatialBatchNormalization(4096,1e-3,false))
@@ -47,7 +47,7 @@ local features = nn.Sequential()
 
    features:add(nn.View(nClasses))
    features:add(nn.LogSoftMax())
- 
+
    local model = features
    return model
 end
